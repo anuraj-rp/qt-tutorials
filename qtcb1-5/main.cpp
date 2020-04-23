de959d79363c0e1a66ac2a4ffa6c23690dde1a21
Anuraj R <anuraj-rp@users.noreply.github.com>
2020-04-23 12:35:42 +0300

    Add docxygen comments

--- qtcb1-5/main.cpp
+++ qtcb1-5/main.cpp
@@ -1,6 +1,13 @@
 #include <QCoreApplication>
 #include <QDebug>
 
+
+/**
+ * @brief main function
+ * @param argc argument1
+ * @param argv argument2
+ * @return int
+ */
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
