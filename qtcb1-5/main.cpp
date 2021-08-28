b9d61844b105778473aa991c2448ce0be0366293
Anuraj R <anuraj-rp@users.noreply.github.com>
2021-08-28 18:41:11 +0300

    qInfo, qDebug, qCritical, qFatal

--- qtcb1-5/main.cpp
+++ qtcb1-5/main.cpp
@@ -1,7 +1,8 @@
 #include <QCoreApplication>
 #include <QDebug>
+#include <iostream>
 
-
+using namespace std;
 /**
  * @brief main function
  * @param argc argument1
@@ -13,6 +14,19 @@ int main(int argc, char *argv[])
     QCoreApplication a(argc, argv);
 
     qInfo() << "Hello World";
+
+    int age;
+    qInfo() << "Enter your age";
+    cin >> age;
+    string name;
+    qInfo() << "Enter you name";
+    cin >> name;
+    qInfo() << "Information";
+    qDebug() << "Debug";
+    qCritical() << "Critical";
+
+    qInfo() << name.c_str() << "is"  << age << "years old";
+    //qFatal("test CRASH !!!!");
 
     return a.exec();
 }
